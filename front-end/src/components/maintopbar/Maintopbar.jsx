131624d7dadf7d2c9ca306670f8e0ae0aa00728b
Ali Muhammad Khowaja <alimuhammad.akesp@gmail.com>
2022-04-06 21:50:41 +0500

    test Commit

---
+++ front-end/src/components/maintopbar/Maintopbar.jsx
@@ -0,0 +1,35 @@
+import "./maintopbar.css";
+//import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+//import { Search, Chat } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+//import { useContext } from "react";
+//import { AuthContext } from "../../context/AuthContext";
+
+export default function Topbar() {
+  //const { user } = useContext(AuthContext);
+  //const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">FANCY WORLD</span>
+        </Link>
+      </div>
+      {/* <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for Existing and New Breeds"
+            className="searchInput"
+          />
+        </div>
+      </div> */}
+     
+        <div className="topbarLinks">
+          <span className="topbarLink">About</span>
+          <span className="topbarLink">Contact US</span>
+        </div>
+     
+    </div>
+  );
+}
